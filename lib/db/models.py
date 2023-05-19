52f6879a89e6abf5ce80e1be7e341ef3f10bc507
Lauren <halpertln@gmail.com>
2023-05-19 14:51:36 -0600

    migrated tables

--- lib/db/models.py
+++ lib/db/models.py
@@ -3,23 +3,15 @@ from sqlalchemy import ForeignKey, Table, Column, Integer, String, DateTime
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///many_to_many.db')
+engine = create_engine('sqlite:///trip.db')
 
 Base = declarative_base()
-
-plane_passenger = Table(
-    'plane_passengers',
-    Base.metadata,
-    Column('plane_id', ForeignKey('planes.id'), primary_key = True),
-    Column('passenger_id', ForeignKey('passenger.id'), primary_key = True),
-    extend_existing = True
-)
 
 
 class Airline(Base):
     __tablename__ = 'airlines'
 
-    id = Column(Integer(), primary_key = True),
+    id = Column(Integer(), primary_key = True)
     airline_name = Column(String())
 
     def __repr__(self):
@@ -28,10 +20,10 @@ class Airline(Base):
 class Route(Base):
     __tablename__ = 'routes'
 
-    id = Column(Integer(), primary_key = True),
-    departure_city = Column(String()),
-    arrival_city = Column(String()),
-    departure_time = Column(DateTime()),
+    id = Column(Integer(), primary_key = True)
+    departure_city = Column(String())
+    arrival_city = Column(String())
+    departure_time = Column(DateTime())
     arrival_time = Column(DateTime())
 
     def __repr__(self):
@@ -41,16 +33,16 @@ class Route(Base):
 class Plane(Base):
     __tablename__ = 'planes'
 
-    id = Column(Integer(), primary_key = True),
-    airline_id = Column(ForeignKey('airlines.id')),
-    route_id = Column(ForeignKey('routes.id')),
-    plane_type = Column(String()),
+    id = Column(Integer(), primary_key = True)
+    airline_id = Column(ForeignKey('airlines.id'))
+    route_id = Column(ForeignKey('routes.id'))
+    plane_type = Column(String())
     passenger_limit = Column(Integer())
 
     airline = relationship('Airline', back_populates = 'planes')
     route = relationship('Route', back_populates = 'planes')
 
-    passengers = relationship('Passenger', secondary = plane_passenger, back_populates = 'planes')
+    
 
     def __repr__(self):
         print(f'Plane, {plane_type}, operated by {airline.airline_name}, is going on route {route.departure_city}-{route.arrival_city} and has a passenger limit of {passenger_limit}. The passengers on this plane are: {passengers}.')
@@ -58,12 +50,15 @@ class Plane(Base):
 class Passenger(Base):
     __tablename__ = 'passengers'
 
-    id = Column(Integer(), primary_key = True),
-    passenger_name = Column(String()),
+    id = Column(Integer(), primary_key = True)
+    passenger_name = Column(String())
     passenger_age = Column(Integer())
+    plane_id = ForeignKey('planes.id')
 
-    planes = relationship('Plane', secondary = plane_passenger, back_populates= 'passengers')
+    plane = relationship ('Plane', back_populates='passengers')
+
+    
 
     def __repr__(self):
-        print(f'Passenger, {passenger_name} is {passenger_age} years old and has id of {id}.')
+        print(f'Passenger, {passenger_name} is {passenger_age} years old and is on plane {plane.plane_type}.')
 
