0754771ff8af6fa6f4a7a211ef5c4ae844362c7f
Lauren <halpertln@gmail.com>
2023-05-27 11:10:28 -0600

    make changes to cli and helpers

--- lib/db/cli.py
+++ lib/db/cli.py
@@ -11,7 +11,10 @@ from models import Airline, Route, Passenger, Plane
 from helpers import (
     retrieve_passenger_info,
     show_menu,
-    view_my_info
+    view_my_info,
+    edit_my_info,
+    update_my_info,
+    update_message
 )
 
 engine = create_engine('sqlite:///trip.db')
@@ -27,10 +30,40 @@ if __name__ == '__main__':
         #  name = input('Please enter your name: ')
     retrieve_passenger_info(name)
     if retrieve_passenger_info(name):
-        print (f'Hello, {name}. What actions would you like to take today?')
-        show_menu()
-        if show_menu() == "view my info":
+        print(f'Hello, {name}.')
+        print('What actions would you like to take today?')
+        menu = input('"view my info", "view my flight info", "change my flight", "cancel my flight", or "exit": ')
+        if menu == 'view my info':
+            print('Here is your info: ')
             view_my_info(name)
+            edit_choice = input('Would you like to edit your info? y/n : ')
+            if edit_choice == 'y':
+                edit = input('Name, Age :')
+                edit
+            elif edit_choice == 'n':
+                # menu = input('"view my info", "view my flight info", "change my flight", "cancel my flight", or "exit": ')
+                # menu
+                print(f'Thanks for visiting {name}. Goodbye!')
+
+    # if retrieve_passenger_info(name):
+    #     print (f'Hello, {name}.')
+    #     show_menu()
+    #     if show_menu() == "view my info":
+    #         view_my_info(name)
+    #         if view_my_info(name) == 'y':
+    #             edit_my_info()
+    #             edited_info = edit_my_info()
+    #             update_my_info(edited_info)
+    #             update_message()
+    #             if update_message() == 'y':
+    #                 show_menu()
+    #             elif update_message() == 'n':
+    #                 edit_my_info()
+    #                 edited_info = edit_my_info()
+    #                 update_my_info(edited_info)
+    #                 update_message()
+    #         elif view_my_info(name) == 'n':
+    #             show_menu()
         
 
     # else:
--- lib/db/helpers.py
+++ lib/db/helpers.py
@@ -22,11 +22,26 @@ def retrieve_passenger_info(input):
     return( passenger[0] )
     
 def show_menu():
+    print('What actions would you like to take today?')
     menu = input('"view my info", "view my flight info", "change my flight", "cancel my flight", or "exit": ')
     return menu
 
 def view_my_info(name):
     my_info = session.query(Passenger).filter(Passenger.passenger_name.like(f'%{name}%')).all()
-    print(f'Here\'s your info... Name: {name} Age: {my_info[0].passenger_age}')
-    option_to_edit = input('Would you like to edit your info? y/n: ')
-    return option_to_edit
+    print(f'Name: {name} Age: {my_info[0].passenger_age}')
+    # option_to_edit = input('Would you like to edit your info? y/n: ')
+    # return option_to_edit
+
+def edit_my_info():
+    edit = input('Name, Age :')
+    return edit
+
+def update_my_info(personal_info):
+    # do something to update the db with the new personal_info
+    pass
+
+def update_message():
+    print('Here is your updated info:')
+    view_my_info()
+    confirm_edited_info= input('Is this correct? y/n')
+    return confirm_edited_info
